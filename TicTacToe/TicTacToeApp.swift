36cfe63e24861af903d9064a9b998e96fa6809a8
Xaver Gruber <xaver.max.gruber@gmail.com>
2022-11-18 18:35:32 +0100

    Swift with Xcode initial project setup

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Xaver Gruber on 18.11.22.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
